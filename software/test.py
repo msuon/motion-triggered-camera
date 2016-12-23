77e76e5ff0f2a880dc993ee44a7d10ad981ca156
Moniroth Suon <msuon@uci.edu>
2016-12-23 23:04:33 +0000

    test camera

--- software/test.py
+++ software/test.py
@@ -1,1 +1,8 @@
-print("something")
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+sleep(2)
+camera.capture("test.jpg")
